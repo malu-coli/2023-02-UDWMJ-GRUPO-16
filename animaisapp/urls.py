d17add0f92a51c6ba216682c9bf1590c4a1225f5
gmarqs <mguilherme760@gmail.com>
2023-11-28 22:27:23 -0300

    feat: init requests

--- animaisapp/urls.py
+++ animaisapp/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('especies/', include('species.urls', namespace='species')),
     path('racas/', include('breeds.urls', namespace='breeds')),
     path('animais/', include('animals.urls', namespace='animals')),
-    path('clientes/', include('clients.urls', namespace= 'clients'))
+    path('clientes/', include('clients.urls', namespace= 'clients')),
+    path('solicitacoes/', include('requests.urls', namespace= 'requests'))
 ]
