9c98531c1b2a212ce9c3b0d5b84726850cd13559
guilhermemoreira <guilhermelompamoreira@gmail.com>
2023-11-29 21:53:35 -0300

    feat: init animals_forms

--- apps/animals/views.py
+++ apps/animals/views.py
@@ -1,9 +1,49 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import AnimalForm
 from .models import Animal
 from rest_framework import viewsets
 from .serializer import AnimalSerializer
 
 class AnimalViewSet(viewsets.ModelViewSet):
     queryset = Animal.objects.all()
-    serializer_class = AnimalSerializer  
+    serializer_class = AnimalSerializer
+
+def add_animal(request):
+    template_name = 'animals/add_animal.html'
+    context = {}
+    if request.method == 'POST':
+        form = AnimalForm(request.POST)
+        if form.is_valid():
+            f = form.save(commit=False)
+            f.save()
+            form.save_m2m()
+            return redirect('animals:list_animals')
+    form = AnimalForm()
+    context['form'] = form
+    return render(request, template_name, context)
+
+def list_animals(request):
+    template_name = 'animals/list_animals.html'
+    animals = Animal.objects.filter()
+    context = {
+        'animals': animals
+    }
+    return render(request, template_name, context)
+
+def edit_animal(request, id_animal):
+    template_name = 'animals/add_animal.html'
+    context ={}
+    animal = get_object_or_404(Animal, id=id_animal)
+    if request.method == 'POST':
+        form = AnimalForm(request.POST, instance=animal)
+        if form.is_valid():
+            form.save()
+            return redirect('animals:list_animals')
+    form = AnimalForm(instance=animal)
+    context['form'] = form
+    return render(request, template_name, context)
+
+def delete_animal(request, id_animal):
+    animal = Animal.objects.get(id=id_animal)
+    animal.delete()
+    return redirect('animals:list_animals')
